84b1fe76f7619f141b9e3bcb543cf84cb6864635
zyz <2718629413@qq.com>
2023-02-11 19:11:22 +0800

    first commit

---
+++ fish/functions/sudo.fish
@@ -0,0 +1,4 @@
+function sudo --description 'alias sudo sudo -E'
+ command sudo -E $argv
+        
+end
---
+++ zsh/aliases.zsh
@@ -0,0 +1,6 @@
+alias c='clear'
+alias l='ls -la'
+alias ra='ranger'
+alias s='neofetch'
+alias sudo='sudo -E'
+alias nvim='sudo nvim'
